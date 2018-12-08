6c33d5a15c402695c3d661b72be749f451dff800
fantaghiro <happy.wangpei@gmail.com>
2018-12-08 21:56:21 +0800

    finished most of the functions, need tracks and localStorage

---
+++ js/config.js
@@ -0,0 +1,106 @@
+var rules = {}; //用来盛放所有的替换规则
+//注意添加规则的时候，除了在下面写了以ru开头的驼峰写法的规则之后，还要把规则添加到rules里面
+
+//匹配规则汇总
+var ruCombineBrokenLines = {
+  'name': '断句段合并',
+  're':  '(.+[^\\.?\\.”"!！。’\'])\\s*\\n', //匹配不是以这些符号结尾的断行
+  'flag': 'g',
+  'f': function(str, brokenLine){
+    return brokenLine.trim() + ' ';
+  },
+  'group': 'lines',
+  'title': '如果段尾不是由句号、问号、感叹号、引号等结尾，那么这一段就会被合并。'
+}
+
+var ruCombineAllLines = {
+  'name': '合并所有段',
+  're': '\\n',
+  'flag': 'g',
+  'f': ' ',
+  'group': 'lines',
+  'title': '将选中的所有段合并为一段。'
+}
+
+var ruAddEmptyLineBetweenLines = {
+  'name': '段间空行',
+  're': '\\n+',
+  'flag': 'g',
+  'f': '\n\n',
+  'group': 'lines',
+  'title': '在段落间空一个空行（段），如果原本段落间有多个空行，那么也统一整理为空一行。'
+}
+
+var ruDeleteEmptyLinesBetweenLines = {
+  'name': '去掉段间空行',
+  're': '\\n+',
+  'flag': 'g',
+  'f': '\n',
+  'group': 'lines',
+  'title': '去除段落间的空行，不论之前段间空了几行空行。'
+}
+
+var ruDeleteBrackets = {
+  'name': '删除括号及其内容',
+  're': '[\(（].*[\)）]',
+  'flag': 'g',
+  'f': '',
+  'group': 'deletes',
+  'title': '删除全角或半角括号以及里面的内容。及时里面没有内容，括号也会被删掉。'
+}
+
+var ruParagraphIndent = {
+  'name': '段首缩进四格',
+  're': '^\\s*(.+)\\s*$', //匹配行内容
+  'flag': 'mg', //启用多行模式
+  'f': function(line, realContent){
+    return '    ' + realContent.trim();
+  },
+  'group': 'spaces',
+  'title': '在每行行首添加四个空格作为缩进。'
+}
+
+var ruDeleteEmptySpacesBetweenAndAfterParagraphs = {
+  'name': '去除段首段尾空格',
+  're': '^\\s*(.+)\\s*$',
+  'flag': 'mg',
+  'f': function(line, realContent){
+    return realContent.trim() || '';
+  },
+  'group': 'spaces',
+  'title': '删除段首和段尾的空格'
+}
+
+var ruDeleteNumAfterLetters = {
+  'name': '删除字母结尾后的数字',
+  're': '([a-zA-Z]+)\\d+\\b',
+  'flag': 'mg',
+  'f': function(wordEndingWithNumber, word){
+    return word;
+  },
+  'group': 'deletes',
+  'title': '删除一个或多个字母后面紧跟的数字，例如Hello4中的4就会被删去。'
+}
+
+rules['ruCombineBrokenLines'] = ruCombineBrokenLines;
+rules['ruParagraphIndent'] = ruParagraphIndent;
+rules['ruDeleteNumAfterLetters'] = ruDeleteNumAfterLetters;
+rules['ruCombineAllLines'] = ruCombineAllLines;
+rules['ruAddEmptyLineBetweenLines'] = ruAddEmptyLineBetweenLines;
+rules['ruDeleteEmptyLinesBetweenLines'] = ruDeleteEmptyLinesBetweenLines;
+rules['ruDeleteEmptySpacesBetweenAndAfterParagraphs'] = ruDeleteEmptySpacesBetweenAndAfterParagraphs;
+rules['ruDeleteBrackets'] = ruDeleteBrackets;
+
+//groups的配置
+var groups = {
+  'lines': '段落处理',
+  'spaces': '空格处理',
+  'deletes': '删除处理',
+  'userDefined': '用户自定义'
+}
+
+//用于保存用户策略
+var userPlan = [ruParagraphIndent, ruDeleteBrackets];
+
+//用于保存是否在粘贴时自动选中粘贴内容
+var useUserPlanOnPaste = false;
--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-//注意页面中button的id命名规则，要与rule的规则对应
-//例如针对ruCombineBrokenLines这样一个rule，button的id要设置为combineBrokenLines
 
 $(function() {
-	var rules = {}; //用来盛放所有的替换规则
+	// var rules = {}; //所有的正则替换规则，已经从config中加载过了
+	// var userPlan = [ruCombineBrokenLines, ruParagraphIndent, ruDeleteNumAfterLetters]; //用来盛放用户策略
+	// var useUserPlanOnPaste = false;
+	// var groups = {
+	// 	'lines': '段落处理',
+	// 	'spaces': '空格处理',
+	// 	'deletes': '删除处理',
+	// 	'userDefined': '用户自定义'
+	// }
 
 	//utils
 	//设置文本选中高亮
@@ -76,54 +82,200 @@ $(function() {
 		resetEditorWidth();
 	})
 
-	//匹配规则汇总
-	var ruCombineBrokenLines = {
-		'name': '断句行合并',
-		're':  '(.+[^\\.?\\.”"!！。’\'])\\s*\\n', //匹配不是以这些符号结尾的断行
-		'flag': 'g',
-		'f': function(str, brokenLine){
-			return brokenLine.trim() + ' ';
+  //生成【替换操作】以上部分的分组按钮
+	var renderGroupButtons = function(rules){
+		var $toolbars = $('#toolbars');
+		var group;
+		var button;
+		var generatedGroups = [];
+
+		var generateGroup = function(en, cn){
+			var group = $('<div class=\"' + en + '\"></div>');
+			var h3 = $('<h3>' + cn + '</h3>');
+			group.prepend(h3);
+			$('#toolbars').prepend(group);
+			return group;
 		}
+		var generateButton = function(ru, rule, group){
+			var reAllNums = /^[0-9]+$/;
+			var id = '';
+			if (reAllNums.test(ru)){ //说明这个rule是用户自定义的
+				id = ru;
+			} else {
+				id = ru[2].toLowerCase() + ru.substring(3);
+			}
+			var button = $('<button class="btn" title=' + rule['title'] + ' id=\"' + id + '\">' + rule['name'] + '</button>');
+			group.append(button);
+			return button;
+		}
+
+		$('#toolbars').find('.userPlan').prevAll().remove();
+
+		for(ru in rules){
+			var groupEn = rules[ru]['group'];
+			var groupCn = groups[groupEn];
+			if (generatedGroups.indexOf(groupEn) == -1){
+				group = generateGroup(groupEn, groupCn);
+				generatedGroups.push(groupEn);
+				button = generateButton(ru, rules[ru], group);
+			} else {
+				group = $('#toolbars' + ' .' + groupEn);
+				button = generateButton(ru, rules[ru], group);
+			}
+		}
+
+		//统一为按钮添加事件
+		$.each($('#toolbars .btn'), function(idx, btn){
+			var id = btn.id;
+			var ruleName = '';
+			var reAllNums = /^\d+$/;
+			if (reAllNums.test(id)){
+				ruleName = id;
+			} else {
+				ruleName = 'ru' + id[0].toUpperCase() + id.substring(1);
+			}
+
+	    var editor = $('#editor').get(0);
+			if (rules[ruleName]){
+	      $('#' + id).on('click', function(){
+	        replaceSelectedText(editor, rules[ruleName]);
+	      })
+			}
+		})
+
+		//控制tooltip的显示
+		var useTooltip = function(){
+			//设置工作区按钮title的显示
+			var offsetX = -10;
+			var offsetY = 30;
+			var newTitle = '';
+			$('#toolbars button').mouseover(function(e){
+				if (this.title == '' || this.title == '用户自定义') {
+					newTitle = '';
+					return;
+				} else {
+					newTitle = this.title;
+				}
+				this.title = '';
+				$('body').append('<div id="tooltip">' + newTitle + '</div>');
+				$('#tooltip').css({
+					'left': (e.pageX + offsetX + 'px'),
+					'top': (e.pageY + offsetY + 'px')
+				}).show();
+			}).mouseout(function(){
+					this.title = newTitle;
+				$('#tooltip').remove();
+			}).mousemove(function(e){
+				if(newTitle == '' || newTitle == '用户自定义') return;
+				$('#tooltip').css({
+					'left': (e.pageX + offsetX + 'px'),
+					'top': (e.pageY + offsetY + 'px')
+				}).show();
+			})
+		}
+
+		useTooltip();
+
+		//控制小工具条的显示与功能
+		var useToolbtn = function(){
+			var offsetX = -10;
+			var offsetY = 20;
+			var btnId = ''
+			//显示
+			$('#toolbars .btn').contextmenu(function(e){
+				return false; //禁用原来的contextmenu
+			})
+
+			$('#toolbars').on('mousedown', function(e){
+				if ($(e.target).hasClass('btn')) {
+					btnId = e.target.id;
+					if (e.button == 2){
+						$('#tooltip').remove();
+						$('#toolbtn').css({
+							'left': (e.pageX + offsetX + 'px'),
+							'top': (e.pageY + offsetY + 'px')
+						}).show();
+					}
+				} else if(e.button == 0){
+					$('#toolbtn').hide(100);
+				}
+			})
+
+			$('.toolbararea').on('scroll', function(){
+				$('#toolbtn').hide();
+			})
+			//功能
+			$('#toolbtn button').eq(1).on('click', function(e){
+				var ruleName = '';
+				var reAllNums = /^\d+$/;
+				if (reAllNums.test(btnId)){
+					ruleName = btnId;
+				} else {
+					ruleName = 'ru' + btnId[0].toUpperCase() + btnId.substring(1);
+				}
+				delete rules[ruleName];
+				$('#toolbtn').hide();
+				renderGroupButtons(rules);
+			})
+
+			$('#toolbtn button').eq(0).on('click', function(e){
+				var ruleName = '';
+				var reAllNums = /^\d+$/;
+				if (reAllNums.test(btnId)){
+					ruleName = btnId;
+				} else {
+					ruleName = 'ru' + btnId[0].toUpperCase() + btnId.substring(1);
+				}
+				var ruTemp = $.extend(true, {}, rules[ruleName]);
+				userPlan.push(ruTemp);
+				$('#toolbtn').hide();
+				renderUserPlan();
+			})
+
+		}
+
+		useToolbtn();
+
 	}
+	renderGroupButtons(rules);
 
-	var ruParagraphIndent = {
-		'name': '段首缩进四格',
-		're': '^\\s*(.+)\\s*$', //匹配行内容
-		'flag': 'mg', //启用多行模式
-		'f': function(line, realContent){
-			return '    ' + realContent.trim();
+	var renderUserPlan = function(){
+		var $userPlanList = $('#userPlan').find('ol');
+		var checkbox = $('#useUserPlanOnPaste').get(0);
+		var $li;
+		$userPlanList.empty();
+		if (userPlan.length !== 0) {
+			userPlan.forEach(function(rule, idx){
+				$li = $('<li>' + rule['name'] + '</li>');
+				$userPlanList.append($li);
+			})
 		}
+		checkbox.checked = useUserPlanOnPaste
 	}
+	renderUserPlan();
 
-	var ruDeleteNumAfterLetters = {
-		'name': '删除字母结尾后的数字',
-		're': '([a-z]+)\\d+',
-		'flag': 'g',
-		'f': function(wordEndingWithNumber, word){
-			return word;
-		}
-	}
-
-	rules['ruCombineBrokenLines'] = ruCombineBrokenLines;
-	rules['ruParagraphIndent'] = ruParagraphIndent;
-	rules['ruDeleteNumAfterLetters'] = ruDeleteNumAfterLetters;
-
-
-	//统一为按钮添加事件
-	$.each($('#toolbars button'), function(idx, btn){
-		var id = btn.id;
-		var ruleName = 'ru' + id[0].toUpperCase() + id.substring(1);
-    var editor = $('#editor').get(0);
-		if (rules[ruleName]){
-      $('#' + id).on('click', function(){
-        replaceSelectedText(editor, rules[ruleName]);
-      })
-		}
+	//点选【粘贴时自动执行用户策略】复选框时的动作
+	$('#useUserPlanOnPaste').on('click', function(e){
+		var bool = e.target.checked;
+		useUserPlanOnPaste = bool;
 	})
 
-	//开始替换按钮的动作
-	$('#executeReplace').on('click', function(){
-    	var editor = $('#editor').get(0);
+	//点击【执行用户策略】按钮的操作
+	$('#executeUserPlan').on('click', function(){
+		if (userPlan.length == 0) return; //用户策略里面没东西，直接返回
+		var editor = $('#editor').get(0);
+		replaceSelectedText(editor, userPlan);
+	})
+
+	//点击【清空用户策略】时候的操作
+	$('#clearUserPlan').on('click', function(){
+		$('#userPlan').find('ol').empty();
+		userPlan = [];
+	})
+
+	//替换操作区域的规则生成
+	var generateRuleFromReplace = function(){
+		var editor = $('#editor').get(0);
 		var pos = $('#paraPosition').val();
 		var originalText = $('#originalText').val();
 		var intendedText = $('#intendedText').val() || '';
@@ -147,19 +299,49 @@ $(function() {
 				flag = 'mg';
 				break;
 		}
-		var ruTemp = {
-			'name': '自定义替换',
+		return {
+			'group': 'userDefined',
+			'name': '',
 			're': reStr,
 			'flag': flag,
-			'f': ''.split.call(intendedText, '\\n').join('\n').split('\$\&').join('$&').split('\$\`').join('$`').split('\$\'').join('$\'')
+			'f': ''.split.call(intendedText, '\\n').join('\n').split('\$\&').join('$&').split('\$\`').join('$`').split('\$\'').join('$\'').split('\\t').join('\t'),
+			'title': '用户自定义'
 		}
+	}
+	//开始替换按钮的动作
+	$('#executeReplace').on('click', function(){
+		var ruTemp = generateRuleFromReplace();
 		replaceSelectedText(editor, ruTemp);
+	})
+
+	//点击【+自定义按钮】时的动作
+	$('#addToUserButtonFromReplace').on('click', function(){
+		var ruTemp = generateRuleFromReplace();
+		ruTemp['name'] = window.prompt('请给这个功能起个名字。') || '自定义替换';
+		var randomId = ('' + Math.random()).replace('.', '');
+		rules[randomId] = ruTemp;
+		renderGroupButtons(rules);
+	})
+
+	//点击【+用户策略】时的动作
+	$('#addToUserPlanFromReplace').on('click', function(){
+		var ruTemp = generateRuleFromReplace();
+		ruTemp['name'] = window.prompt('请给这个功能起个名字。') || '自定义策略';
+		userPlan.push(ruTemp);
+		renderUserPlan();
 	})
 
 	//为替换操作中上面一栏特殊字符按钮添加事件
 	$('#regexCharacters').on('click', function(ev){
-		var prevStr = $('#originalText').val();
-		$('#originalText').val(prevStr + ev.target.dataset.chr).focus().trigger('input');
+		var $originalText = $('#originalText');
+		var originalText = $originalText.get(0);
+		var chr = ev.target.dataset.chr;
+		var posStart = originalText.selectionStart;
+		var prevStr = $originalText.val();
+		var newStr = prevStr.substring(0, posStart) + chr + prevStr.substring(posStart);
+		$originalText.val(newStr).focus().trigger('input');
+		originalText.selectionStart = posStart + chr.length;
+		originalText.selectionEnd = posStart + chr.length;
 	})
 
 	//为替换操作中下面一栏的特殊字符按钮添加事件
@@ -177,7 +359,6 @@ $(function() {
 			}else {
 				elem.style.backgroundColor = '#fff';
 			}
-
 		}
 	})
 
@@ -189,8 +370,9 @@ $(function() {
 		setTimeout(function(){
 			end = $('#editor').prop('selectionEnd');
 			setTextSelected(ev.target, start, end);
-			var rules = [ruCombineBrokenLines, ruParagraphIndent, ruDeleteNumAfterLetters];
-			replaceSelectedText(ev.target, rules);
+			if(useUserPlanOnPaste){ //是否在粘贴时就采用用户策略
+				replaceSelectedText(ev.target, userPlan);
+			}
 			start = ev.target.selectionStart;
 			end = ev.target.selectionEnd;
 			ev.target.value = ev.target.value.replace(/.{1}$/g,  "$&\n");
@@ -198,18 +380,44 @@ $(function() {
 		}, 200)
 	})
 
-	//开始 Hack 按钮的动作
-	$('#executeHack').on('click', function(){
+	// HACK操作区的规则生成
+	var generateRuleFromHack = function(needName){
 		var editor = $('#editor').get(0);
 		var $regex = $('#regex');
 		var $flag = $('#flag');
 		var $function = $('#function');
 		var ruTemp = {
-			'name': '自定义骇客',
+			'name': '',
 			're': $regex.val(),
-			'flag': $flag.val(),
-			'f': new Function('a', 'b', 'c', 'd', 'e', 'f', 'g', $function.val())
+			'flag': $flag.val() || '',
+			'f': new Function('a', 'b', 'c', 'd', 'e', 'f', 'g', $function.val()),
+			'group': 'userDefined'
 		}
+		if(needName && ruTemp['name'] == ''){
+			ruTemp['name'] = window.prompt('请给这个功能起个名字。') || '自定义hack';
+		}
+		return ruTemp;
+	}
+
+	//开始 Hack 按钮的动作
+	$('#executeHack').on('click', function(){
+		var ruTemp = generateRuleFromHack(false);
 		replaceSelectedText(editor, ruTemp);
 	})
+
+	//点击Hack区域中的【+自定义按钮】时的动作
+	$('#addToUserButtonFromHack').on('click', function(){
+		var ruTemp = generateRuleFromHack(true);
+		var randomId = ('' + Math.random()).replace('.', '');
+		rules[randomId] = ruTemp;
+		renderGroupButtons(rules);
+	})
+
+	//点击Hack区域中的【+用户策略】时的动作
+	$('#addToUserPlanFromHack').on('click', function(){
+		var ruTemp = generateRuleFromHack(true);
+		userPlan.push(ruTemp);
+		renderUserPlan();
+	})
+
 })
